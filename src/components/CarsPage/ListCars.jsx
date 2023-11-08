9755d89f631e9153bf2df945bc1b021e76f40022
rianwp <rianwp2015@gmail.com>
2023-11-08 21:08:57 +0700

    adding error handler

--- src/components/CarsPage/ListCars.jsx
+++ src/components/CarsPage/ListCars.jsx
@@ -6,9 +6,14 @@ const ListCars = () => {
 	const { state } = useContext(CarContext)
 	return (
 		<div className="w-[calc(100%+24px)] -ml-3 pb-[40px] pt-[28px] flex flex-row flex-wrap">
-			{state.filteredCars.map((car, index) => {
-				return <Car key={index} car={car} />
-			})}
+			{state.isLoading ? <p>Loading...</p> : null}
+			{!state.isLoading && state.error ? <p>{state.error}</p> : null}
+
+			{!state.isLoading && !state.error
+				? state.filteredCars.map((car, index) => {
+						return <Car key={index} car={car} />
+				  })
+				: null}
 		</div>
 	)
 }
