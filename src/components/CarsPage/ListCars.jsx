c1a3f112b594cb79e243508917a203cb3935d583
rianwp <rianwp2015@gmail.com>
2023-11-08 21:47:37 +0700

    styling error handler

--- src/components/CarsPage/ListCars.jsx
+++ src/components/CarsPage/ListCars.jsx
@@ -5,16 +5,21 @@ import Car from "./Car"
 const ListCars = () => {
 	const { state } = useContext(CarContext)
 	return (
-		<div className="w-[calc(100%+24px)] -ml-3 pb-[40px] pt-[28px] flex flex-row flex-wrap">
-			{state.isLoading ? <p>Loading...</p> : null}
-			{!state.isLoading && state.error ? <p>{state.error}</p> : null}
-
-			{!state.isLoading && !state.error
-				? state.filteredCars.map((car, index) => {
-						return <Car key={index} car={car} />
-				  })
-				: null}
-		</div>
+		<>
+			{state.isLoading ? (
+				<p className="w-full text-center py-10">Loading...</p>
+			) : null}
+			{!state.isLoading && state.error ? (
+				<p className="w-full text-center py-10">{state.error}</p>
+			) : null}
+			<div className="w-[calc(100%+24px)] -ml-3 pb-10 pt-7 flex flex-row flex-wrap">
+				{!state.isLoading && !state.error
+					? state.filteredCars.map((car, index) => {
+							return <Car key={index} car={car} />
+					  })
+					: null}
+			</div>
+		</>
 	)
 }
 
