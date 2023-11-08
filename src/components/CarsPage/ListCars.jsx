bb3fca47d2c4e70d913355a6a1676cd246f798a6
rianwp <rianwp2015@gmail.com>
2023-11-08 20:43:51 +0700

    fixing margin

--- src/components/CarsPage/ListCars.jsx
+++ src/components/CarsPage/ListCars.jsx
@@ -5,7 +5,7 @@ import Car from "./Car"
 const ListCars = () => {
 	const { state } = useContext(CarContext)
 	return (
-		<div className="w-[calc(100%+24px)] -ml-3 py-[40px] flex flex-row flex-wrap">
+		<div className="w-[calc(100%+24px)] -ml-3 pb-[40px] pt-[28px] flex flex-row flex-wrap">
 			{state.filteredCars.map((car, index) => {
 				return <Car key={index} car={car} />
 			})}
